2b26b710702503a7a4e40b12c690c071b9fa3612
Ze Dang <4649tz@gmail.com>
2020-07-22 11:39:44 +0800

    [zedang] feat: finish ex-2

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,13 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let ans = 0;
+  if (string == null) {
+    ans = 0;
+  } else if (prediction == null) {
+    ans = string.length;
+  } else {
+    ans = Array.from(string).reduce((prev, curr) => (prediction(curr) ? prev + 1 : prev), 0);
+  }
+  return ans;
 }
