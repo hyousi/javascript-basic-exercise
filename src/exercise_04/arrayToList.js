8972adba9d91a3b5d0872305665fa2bf04e35812
Ze Dang <4649tz@gmail.com>
2020-07-22 14:00:56 +0800

    [zedang] feat: finish ex-4

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,20 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+
+  if (array == null) {
+    throw new Error('Creating list from undefined array');
+  } else if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  } else {
+    const headNode = { value: array[0], next: null };
+    let ptr1 = headNode;
+    let ptr2 = 1;
+    while (ptr2 < array.length) {
+      ptr1.next = { value: array[ptr2], next: null };
+      ptr1 = ptr1.next;
+      ptr2 += 1;
+    }
+    return headNode;
+  }
 }
