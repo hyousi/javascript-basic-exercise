bbded0c6781c21239bd66684c487f32be7d78b4b
Ze Dang <4649tz@gmail.com>
2020-07-22 15:41:11 +0800

    [zedang] feat: finish ex-7

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,5 +9,12 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (promises.filter(v => !!v && typeof v.then === 'function').length < [...promises].length) {
+    throw new Error('Not all elements are promises.');
+  }
+
+  return Promise.allSettled(promises)
+    .then(results => results.filter(r => r.status === 'rejected'))
+    .then(results => results.length > 0)
+    .then(flag => (flag ? Promise.reject() : Promise.resolve()));
 }
